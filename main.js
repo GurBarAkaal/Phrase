ed189f4eaf29b22e013273d0179272a9bbbbad21
GurBarAkaal <145370236+GurBarAkaal@users.noreply.github.com>
2023-11-22 10:10:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// Complete The Phrase
+
+// Add an Event Listener to the button to run the place Order Function
+document.getElementById("btn1").addEventListener("click", placeOrder1);
+//  Store order code in a function
+function placeOrder1() {
+  // PROCESS
+  let msg = `gets the worm.`;
+  // OUTPUT
+  alert(msg);
+}
+document.getElementById("btn2").addEventListener("click", placeOrder2);
+//  Store order code in a function
+function placeOrder2() {
+  // PROCESS
+  let msg = `the cat.`;
+  // OUTPUT
+  alert(msg);
+}
+document.getElementById("btn3").addEventListener("click", placeOrder3);
+//  Store order code in a function
+function placeOrder3() {
+  // PROCESS
+  let msg = `think alike.`;
+  // OUTPUT
+  alert(msg);
+}
+document.getElementById("btn4").addEventListener("click", placeOrder4);
+//  Store order code in a function
+function placeOrder4() {
+  // PROCESS
+  let msg = `than water.`;
+
+  // OUTPUT
+  alert(msg);
+}
+document.getElementById("btn5").addEventListener("click", placeOrder5);
+//  Store order code in a function
+function placeOrder5() {
+  // PROCESS
+  let msg = `of all evil.`;
+
+  // OUTPUT
+  alert(msg);
+}
